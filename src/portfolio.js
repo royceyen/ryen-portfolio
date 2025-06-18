a9ed8720563f3a574efc4c990f3310e26599a46f
Royce Yen <148026471+royceyen@users.noreply.github.com>
2025-06-17 23:26:42 -0500

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,14 +6,13 @@ const header = {
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Yi Hsiang (Royce) Yen',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'Welcome to my portfolio! Here, you can find data analytics projects and success cases of mine. Showcasing various modeling techniques across industries, highlighting how data-driven insights inform decision-making. Feel free connect with me!',
+  resume: 'https://docs.google.com/document/d/1nThBRx3nC2AIR8frV7aq7nf1nmHhIFO9/edit?usp=sharing&ouid=110751678350728265420&rtpof=true&sd=true',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/royceyen/',
+    github: 'https://github.com/royceyen',
   },
 }
 
@@ -65,7 +64,7 @@ const skills = [
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'yen.royce@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
